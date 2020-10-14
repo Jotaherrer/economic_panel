347110dd8249ee20f19049aac0b1ff18c3b0c81d
JulianHerrera <herrera.ajulian@gmail.com>
2020-10-14 10:45:39 -0300

    update series script

--- series_hacienda.py
+++ series_hacienda.py
@@ -127,42 +127,26 @@ def plot_comparative_lines(pandas_df):
     years = pandas_df.groupby(pandas_df.index.year).count().index
     months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio',
               'Agosto','Septiembre','Octubre','Noviembre','Diciembre']
-    data_dict = {}
-    for y in years:
-        data_dict[y] = pandas_df.loc[str(y)]
 
-    # Create X and Y values for bar plots
-    x_values = []
-    y_values = []
-    for i in range(1, len(data_dict.keys())+1):
-        x_values_sample = [len(years) * element + 0.8*i for element in range(12)]
-        x_values.append(x_values_sample)
+    # Create x and y dataframe values
+    x_values = months * len(years)
+    y_values_sample = pandas_df.values.flatten("F")
 
-    for y in data_dict.keys():
-        y_values_sample = data_dict[y].values
-        y_values_sample = y_values_sample.flatten('F')
-        if y == 2020:
-            length = len(y_values_sample)
-            missings = 12 - length
-            y_values_sample = np.pad(y_values_sample,(0,missings),'constant')
-        y_values.append(y_values_sample)
+    if len(y_values_sample) != len(x_values):
+        length = len(x_values)
+        missings = length - len(y_values_sample)
+        y_values = np.pad(y_values_sample,(0,missings),'constant')
 
     # Create Figure
     fig, ax = plt.subplots(figsize=(14,8))
     ax.set_facecolor((0.7,0.8,0.8,0.9))
-    color_lines = ['steelblue', 'green','peru','salmon','gray', 'yellow', 'yellowgreen', 'lime', '']
-    labels_range = np.arange(int(x_values[0][0])+2,int(x_values[-1][-1])+2,len(years))
+    line_color = ['steelblue']
 
     # Plot lines in for loop
-    for i in range(len(years)):
-        if len(y_values[i]) == len(set(y_values[i])):
-            plt.plot(x_values[i], y_values[i], color=color_lines[i],marker='s',markersize='10',linewidth='3', label=years.values[i])
-        else:
-            y_values_line = y_values[i][y_values[i] != 0]
-            x_values_line = x_values[i][:len(y_values_line)]
-            plt.plot(x_values_line, y_values_line, color=color_lines[i],marker='s',markersize='10',linewidth='3', label=years.values[i])
+    plt.plot(x_values, y_values, color=line_color,marker='s',markersize='10',linewidth='3')
 
     # Setting "x" ticks
+    labels_range = np.arange(1,len(x_values),4)
     ax.set_xticks(labels_range)
     ax.set_xticklabels(months, fontsize='13')
     for tick in ax.get_xticklabels():
@@ -184,7 +168,6 @@ def plot_comparative_lines(pandas_df):
     plt.subplots_adjust(bottom= 0.2, top = 0.98)
     plt.xlabel('Mes',fontsize='13')
     plt.ylim((min,max))
-    plt.legend(loc='best')
     plt.grid()
     plt.show()
 
@@ -234,7 +217,7 @@ if __name__ == '__main__':
     series_titulos = series_ok['serie_titulo']
 
     # Filtro de series mas vistas y plotteo
-    most_viewed_titles = get_most_viewed_series(10, series_ok)
+    most_viewed_titles = get_most_viewed_series(20, series_ok)
     most_viewed_ids = get_most_viewed_ids(10, series_ok)
 
     # Create new df for selected columns
