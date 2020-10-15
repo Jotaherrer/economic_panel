24ea191f628e2dc11f238c0a61ac4f04792c06d7
JulianHerrera <herrera.ajulian@gmail.com>
2020-10-15 19:41:11 -0300

    update bbg script

--- bbg_analysis.py
+++ bbg_analysis.py
@@ -1,16 +1,14 @@
 import pandas as pd
 import openpyxl as xl
+import matplotlib.pyplot as plt
 from series_hacienda import line_plot, plot_comparative_bars
 
 data = pd.ExcelFile('demo bbg.xlsx')
 data.sheet_names
 
 # Bloomberg Tickers
-bbg_tickers = {'exportaciones': 'ARBABEXP Index', 'importaciones': 'ARBABIMP Index',
-               'saldo_comercial':'ARBABAL Index', }
-
-# Example of storing a particular sheet in a df
-df = pd.read_excel(data, data.sheet_names[0], index_col=0, parse_dates=True)
+bbg_tickers = {'exportaciones': 'ARBABEXP Index', 'importaciones': 'ARBABIMP Index', 'saldo_comercial':'ARBABAL Index',
+               'saldo_cc':'ARBPCURR Index', 'saldo_cf':'ARBGFINA Index', 'saldo_ca':'ARBGCAPI Index', ''}
 
 # Store multiple sheets in a dictionary
 sheets_dict = {}
@@ -19,30 +17,45 @@ for sheet in data.sheet_names:
     sheets_dict[sheet] = dataframe
 sheets_dict.keys()
 
-# Test bar plots
-columns = df.columns.values
-exportaciones = df.loc[:,columns[0]].reset_index().set_index('index')
-importaciones = df.loc[:,columns[1]].reset_index().set_index('index')
-trade_balance = df.loc[:,columns[2]].reset_index().set_index('index')
-
-expos1 = exportaciones.loc['2018':'2020',:]
-impos1 = importaciones.loc['2018':'2020',:]
-trade1 = trade_balance.loc['2019':'2020',:]
-
-plot_comparative_bars(expos1)
-plot_comparative_bars(impos1)
-
 # Test line plots
 cols_money = df_money_base.columns.values
 base_total = df_money_base.loc['2019':'2020',cols_money[2]].reset_index().dropna().set_index('index')
 line_plot(base_total)
 
-## Trade balance analysis
+""" Trade balance """
 trade_balance = sheets_dict['trade balance']
 trade_balance_cols = trade_balance.columns.values
 # Filter last 3 years
-expos = trade_balance.loc['2018':'2019',trade_balance_cols[0]].reset_index().set_index('index')
+expos = trade_balance.loc['2018':'2020',trade_balance_cols[0]].reset_index().set_index('index')
 expos.rename(columns={expos.columns.values[0]:'Exportaciones'},inplace=True)
-impos = trade_balance.loc['2018':'2019',trade_balance_cols[1]].reset_index().set_index('index')
+impos = trade_balance.loc['2018':'2020',trade_balance_cols[1]].reset_index().set_index('index')
 impos.rename(columns={trade_balance_cols[1]:'Importaciones'},inplace=True)
-saldo_comercial = trade_balance.loc['2018':'2019',trade_balance_cols[2]].reset_index().set_index('index').rename(columns={trade_balance_cols[0]:'Saldo Balanza Comercial'},inplace=True)
+saldo_comercial = trade_balance.loc['2018':'2020',trade_balance_cols[2]].reset_index().set_index('index')
+saldo_comercial.rename(columns={trade_balance_cols[0]:'Saldo Balanza Comercial'},inplace=True)
+# Plot comparatives
+plot_comparative_bars(expos)
+plot_comparative_bars(impos)
+plot_comparative_bars(saldo_comercial)
+
+""" Public Accounts """
+public_accounts = sheets_dict['public accounts']
+public_accounts.rename(columns={public_accounts.columns.values[0]: 'Saldo Cuenta Corriente',
+                                public_accounts.columns.values[1]:'Saldo Cuenta Financiera',
+                                public_accounts.columns.values[2]:'Saldo Cuenta Capital',
+                                public_accounts.columns.values[3]:'Saldo Error'},inplace=True)
+public_accounts.dropna(how='all', subset = public_accounts.columns.values[1:-2], inplace=True)
+# Filter last 3 years
+saldo_cc = public_accounts.loc["2018":'2020','Saldo Cuenta Corriente'].reset_index().set_index('index')
+saldo_ca = public_accounts.loc["2018":'2020','Saldo Cuenta Capital'].reset_index().set_index('index')
+saldo_cf = public_accounts.loc["2018":'2020','Saldo Cuenta Financiera'].reset_index().set_index('index')
+# Plot trend line
+line_plot(saldo_cc)
+line_plot(saldo_ca)
+line_plot(saldo_cf)
+""" GDP  """
+gdp_sector = sheets_dict['gdp_sector']
+gdp_labels = ['Agro y Ganando', 'Pesca', 'Mineria', 'Manufactura', 'Energía, Gas y Agua', 'Construcción', 'Ventas Retail y Mayoristas',
+              'Hoteles y Restaurantes', 'Transporte y Descarga', 'Intermediación Financiera', 'Real Estate', 'Admin. Pública',
+              'Educación', 'Salud', 'Otros', "Servicio Doméstico"]
+for i in range(len(gdp_sector.columns.values)):
+    gdp_sector.rename(columns={gdp_sector.columns.values[i]:gdp_labels[i]},inplace=True)
