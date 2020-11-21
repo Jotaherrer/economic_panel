c8fd66734082370bd1dc18b5103d5ccb2c30ee43
JulianHerrera <herrera.ajulian@gmail.com>
2020-11-21 18:10:02 -0300

    update pdf parser script

--- pdf_reader.py
+++ pdf_reader.py
@@ -5,10 +5,8 @@ from pdfminer.converter import TextConverter
 from io import StringIO
 from pdfminer.pdfpage import PDFPage
 
-import calendar
 
-
-# Create function to read PDF files (EMAE)
+# Create function to read PDF files
 def get_pdf_file_content(path_to_pdf):
     resource_manager = PDFResourceManager(caching=True)
     out_text=StringIO()
@@ -29,64 +27,6 @@ def get_pdf_file_content(path_to_pdf):
     out_text.close()
 
     return text
-
-
-def get_filtered_data(data_indec, month_start, semester):
-    """
-    """
-    dias = ['LU', 'MA', 'MI', 'JU', 'VI']
-    meses_1 = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio']
-    meses_2 = ['Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
-
-    if semester == 2:
-        for m in meses_2:
-            if month_start == 'Julio':
-                mes_inicio = data_indec.find(month_start)
-                mes_fin = data_indec.find(meses_2[1])
-            elif month_start == 'Agosto':
-                mes_inicio = data_indec.find(month_start)
-                mes_fin = data_indec.find(meses_2[2])
-            elif month_start == 'Septiembre':
-                mes_inicio = data_indec.find(month_start)
-                mes_fin = data_indec.find(meses_2[3])
-            elif month_start == 'Octubre':
-                mes_inicio = data_indec.find(month_start)
-                mes_fin = data_indec.find(meses_2[4])
-            elif month_start == 'Noviembre':
-                mes_inicio = data_indec.find(month_start)
-                mes_fin = data_indec.find(meses_2[5])
-            elif month_start == 'Diciembre':
-                mes_inicio = data_indec.find(month_start)
-        info = data_indec[mes_inicio:mes_fin]
-
-
-    elif semester == 1:
-        for m in meses_1:
-            if month_start == 'Enero':
-                mes_inicio = data_indec.find(month_start)
-                mes_fin = data_indec.find(meses_1[1])
-            elif month_start == 'Febrero':
-                mes_inicio = data_indec.find(month_start)
-                mes_fin = data_indec.find(meses_1[2])
-            elif month_start == 'Marzo':
-                mes_inicio = data_indec.find(month_start)
-                mes_fin = data_indec.find(meses_1[3])
-            elif month_start == 'Abril':
-                mes_inicio = data_indec.find(month_start)
-                mes_fin = data_indec.find(meses_1[4])
-            elif month_start == 'Mayo':
-                mes_inicio = data_indec.find(month_start)
-                mes_fin = data_indec.find(meses_1[5])
-            elif month_start == 'Junio':
-                mes_inicio = data_indec.find(month_start)
-        info = data_indec[mes_inicio:mes_fin]
-
-    elif semester is not type(int):
-        print('Pasa un número de semestre en formato numérico')
-
-
-    return info
-
 
 
 if __name__ == '__main__':
